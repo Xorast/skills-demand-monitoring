747992c255627b6d20217a4d597f6dbe1ceafa68
Xorast <xavier.astor.fontaine@gmail.com>
2019-09-08 14:10:03 +0100

    Stored only news adds

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # APP
+
 import logging
 from search import Search
 import mongo
@@ -9,4 +10,15 @@ logging.basicConfig(level=logging.DEBUG)
 
 python_dev = Search(**indeed, query={'kw': 'python+developer', 'location': 'Dublin'})
 results = python_dev.go()
-mongo.collections['indeed'].insert_many(results)
+found_ads = {ad['id_'] for ad in results}
+
+stored_ads = mongo.collections["indeed"].find({}, {"id_": 1, "_id": 0})
+stored_ads = {ad['id_'] for ad in stored_ads}
+
+new_ads = found_ads - stored_ads
+new_results = [ad for ad in results if ad['id_'] in new_ads]
+
+print("TEST")
+print(new_results)
+# mongo.collections['indeed'].insert_many(new_results)
+
